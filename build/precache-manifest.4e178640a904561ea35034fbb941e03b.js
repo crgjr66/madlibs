9b625b2c09de396f80dab23e7ce7ad3ffcfac278
crgjr66 <chuckgilbreth@yahoo.com>
2020-10-16 11:03:42 -0700

    add deploy

---
+++ build/precache-manifest.4e178640a904561ea35034fbb941e03b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9826b2f99a3e1325fb2daad88aaa1b",
+    "url": "/madlibs/index.html"
+  },
+  {
+    "revision": "67f73a332f6be655c850",
+    "url": "/madlibs/static/css/main.c5ee5879.chunk.css"
+  },
+  {
+    "revision": "b90916001abfe03d1352",
+    "url": "/madlibs/static/js/2.9b97cd1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/madlibs/static/js/2.9b97cd1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f73a332f6be655c850",
+    "url": "/madlibs/static/js/main.1a73382f.chunk.js"
+  },
+  {
+    "revision": "f6fd9aa469cb3a092a02",
+    "url": "/madlibs/static/js/runtime-main.f30a53cb.js"
+  },
+  {
+    "revision": "7fad95ad9c4deb45905312542eeca9ed",
+    "url": "/madlibs/static/media/madlibs.7fad95ad.png"
+  }
+]);
